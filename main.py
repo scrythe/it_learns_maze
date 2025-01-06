56eb3441cb31401179db140a521a0ef7819642c9
Magomed Alimkhanov <scrythepvp@gmail.com>
2025-01-06 19:08:50 +0100

    feat: make best ai runnable

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def eval_genomes(genomes: list[Any], config):
         if genome[1].fitness == None:
             return -100
         return genome[1].fitness
+
     best_genome = max(genomes, key=best_genome_max)
 
     print(best_genome)
@@ -52,10 +53,34 @@ def run(config_file: str):
         pickle.dump(winner, f)
 
 
+def test_ai(config_file: str):
+    config = neat.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_file,
+    )
+    with open("best.pickle", "rb") as f:
+        winner = pickle.load(f)
+    # input("")
+    while game.running:
+        game.setup([[0, winner]], config, [0])
+        while len(game.players):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game.running = False
+            game.update()
+            game.draw()
+            game.clock.tick(200)
+        game.round = 26
+
+
 if __name__ == "__main__":
     game = Game()
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
     # import cProfile
     # cProfile.run('run(config_path)')
-    run(config_path)
+    # run(config_path)
+    test_ai(config_path)
